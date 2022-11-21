4e418da70a74ee30d7242f8289bad9c286927dd9
tomms25 <tommasobaccano@gmail.com>
2022-11-21 16:55:59 +0100

    Esercizio terminato

--- js/main.js
+++ js/main.js
@@ -1,96 +1,49 @@
-
 // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta (se avete fatto bonus di ieri e partite da li, sennò range rimane di base 1-100): le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 // In seguito l’utente clicca su una cella:
 // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
 // Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
+// Definisco le variabili 
+const contenitore = document.getElementById('container');
+// console.log(contenitore);
 
+// Definisco il bottone che genererà a griglia
+const button = document.getElementById('btn');
 
+button.addEventListener('click',
+  function () {
+  
+    contenitore.innerHTML = '';
+    
+    console.clear();
 
+    // Definisco il ciclo for
+    for (let i = 0; i < 100; i++){
+      let nuovoElemento = creaSquare();
 
+      nuovoElemento.append([i + 1]);
 
-const btn = document.getElementById("btn");
+      nuovoElemento.addEventListener('click',
+        function () {
+          nuovoElemento.classList.add('square-click');
 
-// Definisco il container
-
-const myContainer = document.getElementById("container");
-
-console.log(myContainer);
-
-// creo l'elemento bottone per la funzione
-
-
-btn.addEventListener ("click",
-
-
-    // Creo la funzione relativa al bottone
-
-    function() {
-        myContainer.innerHTML = '';
-        console.clear();
-
-        // Creazione della griglia 10x10 tramite ciclo for e funzione
-        // Inizio dal ciclo for
-
-        for (let i = 1; i <= 100; i++ ){
-
-            // creo l'elemento per la funzione
-            let nuovoElemento = createBoxGrid(i); 
-
-
-            nuovoElemento.addEventListener("click",
-            
-                function() {
-                    // toggle mi permette di aggiungere o togliere l'elemento aggiunto ad ogni click
-                    nuovoElemento.classList.toggle("clicked");
-
-                    // Visualizzare in console la cella cliccata
-                    console.log("Cella:", i);
-                }
-
-            )
-
-
-            myContainer.appendChild(nuovoElemento);
-        
-
-
-
+          console.log('Hai cliccato la cella:', i + 1);
         }
+      )
 
-
+      contenitore.appendChild(nuovoElemento);
     }
-
-
-
-
-
-
+  }
 )
 
+// Creo la funzione per creare il div e la classe associata
+function creaSquare() {
+  const elemento = document.createElement('div');
+  elemento.classList.add('square');
 
-
-
-
-
-
-// Funzione
-
-function createBoxGrid (index){
-
-    // Creare un elemento div e la sua classe relativa
-    const elementBox = document.createElement("div");
-
-    elementBox.classList.add("celle");
-    // console.log(elementBox);
-
-    // Creo la numerazione progressiva delle celle
-    elementBox.innerHTML = index;
-
-
-    // ritornarlo
-    return elementBox;
-
+  // console.log(elemento);
+  return elemento;
 }
